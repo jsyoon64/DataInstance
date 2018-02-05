57082ede3da7b65267519058f8395b7ef6df4c33
jsyoon <jsyoon@shinwooend.co.kr>
2018-02-05 10:47:17 +0900

    Create Data binding ...

---
+++ app/src/main/java/com/jsyoon/datainstance/Data/SettingData.java
@@ -0,0 +1,60 @@
+package com.jsyoon.datainstance.Data;
+
+import android.content.Context;
+import android.databinding.Bindable;
+import android.databinding.BaseObservable;
+
+import com.jsyoon.datainstance.BR;
+
+
+public class SettingData extends BaseObservable {
+    Context context;
+
+    private boolean setting1;
+    private boolean setting2;
+    private String colorstring;
+    private int textcolor;
+    private int textsize;
+
+    @Bindable
+    public boolean getSetting1() {
+        return setting1;
+    }
+    @Bindable
+    public boolean getSetting2() {
+        return setting2;
+    }
+    @Bindable
+    public String getColorString() {
+        return colorstring;
+    }
+    @Bindable
+    public int getTextColor() {
+        return textcolor;
+    }
+    @Bindable
+    public int gettextSize() {
+        return textsize;
+    }
+
+    public void setSetting1(boolean val) {
+        setting1 = val;
+        notifyPropertyChanged(BR.setting1);
+    }
+    public void setSetting2(boolean val) {
+        setting2 = val;
+        notifyPropertyChanged(BR.setting2);
+    }
+    public void setColorString(String col) {
+        colorstring=col;
+        notifyPropertyChanged(BR.colorString);
+    }
+    public void setTextColor(int color) {
+        textcolor = color;
+        notifyPropertyChanged(BR.textColor);
+    }
+    public void setTextSize(int size) {
+        textsize = size;
+        notifyPropertyChanged(BR.textSize);
+    }
+}
---
+++ app/src/main/java/com/jsyoon/datainstance/MainActivity.java
@@ -0,0 +1,22 @@
+package com.jsyoon.datainstance;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jsyoon.datainstance.Data.SettingData;
+import com.jsyoon.datainstance.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    SettingData data=new SettingData();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        binding.setData(data);
+
+    }
+}

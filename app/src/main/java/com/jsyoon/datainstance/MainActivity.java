4ae3690f4164bb2a7ad5ab92ec8776d2eee46b1e
jsyoon <jsyoon@shinwooend.co.kr>
2018-02-05 11:11:55 +0900

    1. Setting 화면과 연결 2. Setting 화면의 Summary 기능 구현

--- app/src/main/java/com/jsyoon/datainstance/MainActivity.java
+++ app/src/main/java/com/jsyoon/datainstance/MainActivity.java
@@ -1,8 +1,11 @@
 package com.jsyoon.datainstance;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jsyoon.datainstance.Data.SettingData;
 import com.jsyoon.datainstance.databinding.ActivityMainBinding;
@@ -18,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
         binding.setData(data);
 
+        binding.option.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent i = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

a239f9694c991d6a8b4b6ce201d280d4c1876414
jsyoon <jsyoon@shinwooend.co.kr>
2018-02-05 11:03:36 +0900

    1. New Resource file에서 root element를 PreferenceScreen 선택후 항목 추가 2. Setting 화면을 위한 New Activity 추가

---
+++ app/src/main/java/com/jsyoon/datainstance/SettingActivity.java
@@ -0,0 +1,12 @@
+package com.jsyoon.datainstance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
